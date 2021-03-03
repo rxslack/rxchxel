e52f517b3e8c73e8c924302366b6160d7974a77b
Rachael <57686060+rxchxel@users.noreply.github.com>
2021-03-03 20:56:19 +0000

    New Bees app, basics now built

---
+++ website/src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+export const Footer = () => {
+    return (
+        <footer>
+            <p> Created by <a href = 'https://github.com/rxchxel' target='_blank'> rxchxel - 2021 </a></p>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ website/src/pages/bees/bees.jsx
@@ -0,0 +1,16 @@
+import { Header } from '../../components/header';
+import { Footer } from '../../components/footer';
+import { Beehive } from '../../components/beehive';
+
+
+export const Bees = () => {
+    return (
+        <body>
+        <Header />
+        <Beehive />
+        <Footer />
+        </body>
+        )
+}
+
+export default Bees
